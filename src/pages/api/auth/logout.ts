302a71d790def538bdc628ff189d3e119daeeb0c
corlys <duscae047@gmail.com>
2022-12-02 19:16:11 +0700

    feat: new nonce

--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -1,16 +1,36 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { withIronSessionApiRoute } from "iron-session/next";
-import { ironOptions } from "@/config/cookie-config";
 import { signOut } from "firebase/auth";
-import { firebaseAuth } from "@/config/firebase-config";
+import { generateNonce } from "siwe";
+import { ironOptions } from "@/config/cookie-config";
+import { firebaseAuth, firebaseAdminApp } from "@/config/firebase-config";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     if (req.method !== "POST")
       return res.status(400).json({ error: "wrong_method" });
+
+    const userDoc = await firebaseAdminApp
+      .firestore()
+      .collection("users")
+      .doc(req.session.siwe?.address || "")
+      .get();
+
+    if (userDoc.exists) {
+      const newNonce = generateNonce();
+      // Associate the nonce with that user
+      await firebaseAdminApp
+        .firestore()
+        .collection("users")
+        .doc(req.session.siwe?.address || "")
+        .set({
+          nonce: newNonce,
+        });
+    }
+
     await signOut(firebaseAuth);
     req.session.destroy();
-    console.log(req.session)
+
     return res.status(200).json({
       message: "ok",
     });
